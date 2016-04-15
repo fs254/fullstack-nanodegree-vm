be00f543a14b451474d7619e26412b7e3ebac628
fs254 <fs254@njit.edu>
2016-04-14 23:23:47 -0400

    Update database_setup.py

--- vagrant/database_setup.py
+++ vagrant/database_setup.py
@@ -4,4 +4,25 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import creat_engine
 Base = declaretive_base()
+####beginning line####
+
+class Restaurant(Base):
+  _tablename_ = 'restaurant'
+  name = Column(String(80), nullable = False)
+  id = Column(Integer, primary_key = True)
+  
+class MenuItem(Base):
+  _tablename_ = 'menu_item'
+  name = Column(String(80), nullable = False)
+  id = Column(Integer, primary_key = True)
+  course = Column(String(250))
+  description = Column(String(250))
+  price = Column(String(8))
+  restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
+  restaurant = relationship(Restaurant)
+  
+  
+  
+####end line####
 engine = creat_engine ( sqlite:///restaurantmenu.db')
+Base.metabata.create_all(engine)
